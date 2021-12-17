25f628323707731b7c5f6af09531a720d1f44e22
pvicugna11 <pvicugna11@gmail.com>
2021-12-17 11:46:44 +0900

    Object Pooling

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,15 +7,29 @@ public class DestroyOutOfBounds : MonoBehaviour
     private float topBound = 30;
     private float lowerBound = -10;
 
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
     void Update()
     {
         if (transform.position.z > topBound)
         {
-            Destroy(gameObject);
-        } else if (transform.position.z < lowerBound)
+            // Instead of destroying the projectile when it leaves the screen
+            //Destroy(gameObject);
+
+            // Just deactivate it
+            gameObject.SetActive(false);
+
+        }
+        else if (transform.position.z < lowerBound)
         {
             Debug.Log("Game Over!");
             Destroy(gameObject);
         }
+
     }
 }
--- Assets/Scripts/DetectCollisions.cs
+++ Assets/Scripts/DetectCollisions.cs
@@ -1,12 +1,17 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DetectCollisions : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other)
     {
+        // Instead of destroying the projectile when it collides with an animal
+        //Destroy(other.gameObject); 
+
+        // Just deactivate the food and destroy the animal
+        other.gameObject.SetActive(false);
         Destroy(gameObject);
-        Destroy(other.gameObject);
     }
+
 }
